8b5e6099526c17e6ef6491ef6684039293cef7d9
Patrik <prichtarik27@gmail.com>
2022-09-02 20:57:05 +0200

    fix(fcm): fcm module add config to ejabber.yml, new api

--- src/mod_fcm.erl
+++ src/mod_fcm.erl
@@ -2,36 +2,41 @@
 -author("mrDoctorWho").
 
 -include("logger.hrl").
-%% -include("xmpp.hrl").
+-include("xmpp.hrl").
 
 -behaviour(gen_mod).
 
--export([start/2, stop/1, mod_options/1, depends/2, send/2]).
+-export([start/2, stop/1, mod_options/1, depends/2, send/2, mod_opt_type/1]).
 
--define(URL_FCM_BRIDGE, "http://172.17.0.1:3456").
 -define(CONTENT_TYPE, "application/json").
 
 start(Host, Opts) ->
     ?DEBUG("mod_fcm started", []),
-    %% ?DEBUG("Host: ~p, URL : ~p", [Host, gen_mod:get_module_opt(Host, ?MODULE, url_send, fun(V) -> V end, undefined)]),
+    ?DEBUG("Host: ~p, URL : ~p", [Host, gen_mod:get_module_opt(Host, ?MODULE, post_url)]),
+    ?DEBUG("Host: ~p, Token : ~p", [Host, gen_mod:get_module_opt(Host, ?MODULE, auth_token)]),
     %% send(Host, {[{tokens, [<<"token1">>, <<"token2">>]},{data, {[{del_ids, [<<"id1">>, <<"id2">>]}]}}]}),
   ok.
 
 stop(_Host) ->
-    ?DEBUG("mod_fcm stopped", [])
-    .
+    ?DEBUG("mod_fcm stopped", []).
 
 depends(_Host, _Opts) ->
 [].
 
 mod_options(_Host) ->
-[].
+[{ post_url, <<"http://xmpp.elenty.com:3456">> },
+    { auth_token, <<"">>}].
+
+mod_opt_type(post_url) ->
+    fun iolist_to_binary/1;
+mod_opt_type(auth_token) ->
+    fun iolist_to_binary/1.
 
 send(Host, Payload) ->
-    ?DEBUG("Send - payload: ~p, host: ~p", [Payload, Host]),
-    %% URL_FCM_BRIDGE = erlang:binary_to_list(gen_mod:get_module_opt(Host, ?MODULE, url_send, fun(V) -> V end, undefined)),
-    ?DEBUG("url: ~p", [?URL_FCM_BRIDGE]),
-    API_KEY = "key=dsjajdsadoi01002-3-ksdkaskk2",
+
+    URL = erlang:binary_to_list(gen_mod:get_module_opt(Host, ?MODULE, post_url)),
+
+    API_KEY = "key=" ++ binary_to_list(gen_mod:get_module_opt(Host, ?MODULE, auth_token)),
     Header = [{"Authorization", API_KEY}, {"Content-Type", ?CONTENT_TYPE}],
     Body = try jiffy:encode(Payload) of
            BodyDec ->
@@ -45,7 +50,7 @@ send(Host, Payload) ->
     ?DEBUG("Body: ~p", [Body]),
     ssl:start(),
    	application:start(inets),
-   	{ok, RawResponse} = httpc:request(post, {?URL_FCM_BRIDGE, Header, ?CONTENT_TYPE, Body}, [{timeout, 5000}], []),
+   	{ok, RawResponse} = httpc:request(post, {URL, Header, ?CONTENT_TYPE, Body}, [{timeout, 5000}], []),
     ?DEBUG("RawResp ~p", [RawResponse]),
    	{{_, SCode, Status}, ResponseBody} = {element(1, RawResponse), element(3, RawResponse)},
    	%% TODO: Errors 5xx
